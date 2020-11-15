eb2ebc7443a82adbc271d7515606503f40bacecb
Lorenzo Rossiman <20031485@studenti.uniupo.it>
2020-11-15 21:50:12 +0100

    strings fixed + radio buttons fixed

--- app/src/main/java/upo/mobileapplications1/rockpaperscissorsapp/MainActivity.kt
+++ app/src/main/java/upo/mobileapplications1/rockpaperscissorsapp/MainActivity.kt
@@ -13,24 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //create a Computer object which extracts values from 0 to 2
         val computer = Computer()
 
+        //get all radioButtons
         val rockButton = findViewById<RadioButton>(R.id.rock)           //0
         val paperButton = findViewById<RadioButton>(R.id.paper)         //1
         val scissorsButton = findViewById<RadioButton>(R.id.scissors)   //2
 
         val playButton = findViewById<Button>(R.id.playbutton)
+
+        //implement playButton's listener
         playButton.setOnClickListener {
-            //se non è stato selezionato nulla
-            if(!rockButton.isEnabled && !paperButton.isEnabled && !scissorsButton.isEnabled){
-                Toast.makeText(this, R.string.invalid, Toast.LENGTH_LONG)
+
+            //if no radioButton has been selected
+            if(!rockButton.isChecked && !paperButton.isChecked && !scissorsButton.isChecked){
+
+                //TODO: remember .show() at the end!!!
+                Toast.makeText(this, getString(R.string.invalid), Toast.LENGTH_LONG).show()
             }
 
-            //se invece è stata selezionata una opzione
+            //if an option has been selected
             else{
                 val computersPick = findViewById<TextView>(R.id.computerschoice)
 
-                //estraggo un valore da 0 a 2
+                //extract an integer from 0 to 2
                 val computersChoice = computer.extract()
                 when(computersChoice){
                     0 -> {
@@ -44,35 +51,37 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
 
-                val choice = if(rockButton.isEnabled){
+                //check which radioButton has been checked
+                val choice = if(rockButton.isChecked){
                     0
                 }
-                else if(paperButton.isEnabled){
+                else if(paperButton.isChecked){
                     1
                 }
-                else {
+                else if(scissorsButton.isChecked){
                     2
+                }
+                else{
+                    -1
                 }
 
                 val result = findViewById<TextView>(R.id.result)
 
-                //pareggio
+                //tie
                 if(choice == computersChoice){
                     result.setTextColor(Color.parseColor("#FFFF00"))
-                    //result.text = R.string.tie.toString()
                     result.text = getString(R.string.tie)
                 }
-                //sconfitta
+                //defeat
                 else if((computersChoice == 1 && choice == 0)
                     || (computersChoice == 2 && choice == 1)
                     || (computersChoice == 0 && choice == 2)){
                     result.setTextColor(Color.parseColor("#FF0000"))
                     result.text = getString(R.string.loss)
                 }
-                //vittoria
+                //victory
                 else{
                     result.setTextColor(Color.parseColor("#00FF00"))
-                    //result.text = R.string.victory.toString()
                     result.text = getString(R.string.victory)
                 }
             }
